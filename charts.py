3940704f83c680f0a621cd37b3a5355261b50e2b
Daniel-Rumona <danielrumona@gmail.com>
2024-08-18 22:29:44 +0200

    Update charts.py

--- charts.py
+++ charts.py
@@ -174,7 +174,7 @@ def create_highcharts_item_chart_unique(df, location, type, title="Main Sector F
     data = []
     short_names = {
         'Agriculture': 'Agric',
-        'Construction': 'Constr',
+        'Construction': 'Con',
         'Finance': 'Fin',
         'Manufacturing': 'Man',
         'Mining': 'Min',
